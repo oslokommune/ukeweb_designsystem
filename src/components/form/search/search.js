b8dd30df2178471a2c0e30f5e8c4b4193b2b176b
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-06-02 23:03:16 +0200

    Added aria to combobox

--- src/components/form/search/search.js
+++ src/components/form/search/search.js
@@ -3,10 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   if (check) {
     check.addEventListener('change', function () {
       let dropdown = document.querySelector('.osg-search__dropdown');
+      let searchForm = document.querySelector('#osg-search__form');
+
       if (this.checked) {
         dropdown.style.display = 'block';
+        searchForm.setAttribute('aria-expanded', 'true');
       } else {
         dropdown.style.display = 'none';
+        searchForm.setAttribute('aria-expanded', 'false');
       }
     });
   }
