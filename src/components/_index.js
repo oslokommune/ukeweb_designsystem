1fabe92d487695a34fb684c1abd90a472f91e741
Lars Boldt <boldt.lars@gmail.com>
2021-06-02 12:56:39 +0200

    adds collapsible and refactors href on links

--- src/components/_index.js
+++ src/components/_index.js
@@ -1,3 +1,4 @@
+import './collapsible/collapsible.js'
+import './image_carousel/image_carousel.js'
 import './form/search/search.js'
 import './form/search_seasons/search_seasons.js'
-import './image_carousel/image_carousel.js'
---
+++ src/components/collapsible/collapsible.md
@@ -0,0 +1,1 @@
+Triggers get the `.osg-collapsible__trigger--expanded` modifier when content is expanded.
