ffc82406c6ec46b7bf1d373fb95ff7d167ca33d9
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-06-09 11:45:38 +0200

    Added modal component

---
+++ src/components/modal/modal.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = document.querySelector('.osg-modal');
+  const modalOpen = document.querySelector('.osg-modal__open');
+  const modalClose = document.querySelector('.osg-button--circle');
+  const tabKey = 9;
+
+  function toggleModal () {
+    modal.classList.toggle('osg-modal--open');
+
+    (modal.getAttribute('aria-hidden') === 'true')
+    ? modal.setAttribute('aria-hidden', 'false')
+    : modal.setAttribute('aria-hidden', 'true');
+  }
+
+  function trapTabKey (obj, event) {
+    debugger;
+    if (event.keyCode == tabKey) {
+      console.log('tab was pressed');
+    }
+  }
+
+  modalOpen.addEventListener('click', toggleModal, false);
+  modalClose.addEventListener('click', toggleModal, false);
+  modal.addEventListener('keydown', trapTabKey, false);
+});
