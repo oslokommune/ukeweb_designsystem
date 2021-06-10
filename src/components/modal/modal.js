8834ba5e98d204dbd5fa26b8500dec3c78f01a48
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-06-10 10:49:54 +0200

    Check if the modal exists before running the event listener

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,8 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
   const modal = document.querySelector('.osg-modal');
+
+  if (!modal) {
+    return;
+  }
+
   const modalOpen = document.querySelector('.osg-modal__open');
   const modalClose = document.querySelector('.osg-button--circle');
-  const tabKey = 9;
+
+  function trapFocus(element) {
+    var currentElement = element.currentTarget;
+    var focusableEls = currentElement.querySelectorAll('a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input[type="text"]:not([disabled]), input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), select:not([disabled])');
+    var firstFocusableEl = focusableEls[0];
+    var lastFocusableEl = focusableEls[focusableEls.length - 1];
+    var KEYCODE_TAB = 9;
+
+    currentElement.addEventListener('keydown', function(e) {
+      var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+      if (!isTabPressed) {
+        return;
+      }
+
+      if (e.shiftKey) /* shift + tab */ {
+        if (document.activeElement === firstFocusableEl) {
+          lastFocusableEl.focus();
+            e.preventDefault();
+          }
+        } else { /* tab */
+        if (document.activeElement === lastFocusableEl) {
+          firstFocusableEl.focus();
+            e.preventDefault();
+          }
+        }
+    });
+  }
 
   function toggleModal () {
     modal.classList.toggle('osg-modal--open');
@@ -14,5 +46,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   modalOpen.addEventListener('click', toggleModal, false);
   modalClose.addEventListener('click', toggleModal, false);
-  modal.addEventListener('keydown', trapTabKey, false);
+  modal.addEventListener('keydown', trapFocus.bind(modal), false);
+
 });
