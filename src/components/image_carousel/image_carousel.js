45a1dae5372381b40ec18585b0ad3e0177eeefb8
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-06-22 22:36:34 +0200

    Remove duplicated carousel

--- src/components/image_carousel/image_carousel.js
+++ src/components/image_carousel/image_carousel.js
@@ -1,12 +1,8 @@
 import ImageCarousel from './image_carousel.vue';
 
 document.addEventListener('DOMContentLoaded', function () {
-  if (document.getElementById('vue1')) {
+  if (document.getElementById('vue')) {
     Vue.component('osg-image-carousel', ImageCarousel);
-    new Vue({ el: '#vue1' });
-  }
-  if (document.getElementById('vue2')) {
-    Vue.component('osg-image-carousel', ImageCarousel);
-    new Vue({ el: '#vue2' });
+    new Vue({ el: '#vue' });
   }
 });
