f3a9a3609d57ccd60589cdc2535911b67ce05d4b
Lars Boldt <boldt.lars@gmail.com>
2021-07-05 11:28:20 +0200

    fixes several issues

--- src/components/form/search_seasons/search_seasons.js
+++ src/components/form/search_seasons/search_seasons.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let check = document.getElementById('components-form-search-seasons-dropdown');
-  if (check) {
-    check.addEventListener('change', function () {
-      let dropdowns = document.querySelectorAll('.osg-search-seasons__dropdown');
-      let searchForms = document.querySelectorAll('.osg-search-seasons__input-wrapper');
+  let searchSeasonsDropdownTriggers = document.querySelectorAll('.components-form-search-seasons-dropdown');
+  if (searchSeasonsDropdownTriggers) {
+    searchSeasonsDropdownTriggers.forEach(trigger => {
+      trigger.addEventListener('change', function () {
+        let dropdowns = document.querySelectorAll('.osg-search-seasons__dropdown');
+        let searchForms = document.querySelectorAll('.osg-search-seasons__input-wrapper');
 
-      dropdowns.forEach((dropdown) => {
-        dropdown.style.display = this.checked ? 'block' : 'none';
-      });
+        dropdowns.forEach((dropdown) => {
+          dropdown.style.display = this.checked ? 'block' : 'none';
+        });
 
-      searchForms.forEach((searchForm) => {
-        this.checked
-        ? searchForm.setAttribute('aria-expanded', 'true')
-        : searchForm.setAttribute('aria-expanded', 'false')
+        searchForms.forEach((searchForm) => {
+          this.checked
+            ? searchForm.setAttribute('aria-expanded', 'true')
+            : searchForm.setAttribute('aria-expanded', 'false')
+        });
       });
     });
   }
