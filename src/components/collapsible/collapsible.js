1fe87297bdac2deee75a406d903a9fb22b27a593
Lars Boldt <boldt.lars@gmail.com>
2021-07-06 14:48:53 +0200

    updates markup for new collapsible markup

--- src/components/collapsible/collapsible.js
+++ src/components/collapsible/collapsible.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const collapsibleTriggers = document.querySelectorAll('.osg-collapsible__trigger');
+  const collapsibleTriggers = document.querySelectorAll('.osg-collapsible-trigger');
   collapsibleTriggers.forEach((item) => {
     item.addEventListener('click', (e) => {
       e.preventDefault();
       const collapsible = document.getElementById(e.target.getAttribute('aria-controls'));
       if (collapsible) {
-        collapsible.classList.toggle('osg-collapsible--collapsed');
+        collapsible.classList.toggle('osg-collapsible-content--collapsed');
 
         e.target.setAttribute('aria-expanded',
-          collapsible.classList.contains('osg-collapsible--collapsed') ? false : true
+          collapsible.classList.contains('osg-collapsible-content--collapsed') ? false : true
         );
-        e.target.classList.toggle('osg-collapsible__trigger--expanded',
-          collapsible.classList.contains('osg-collapsible--collapsed') ? false : true
+        e.target.classList.toggle('osg-collapsible-trigger--expanded',
+          collapsible.classList.contains('osg-collapsible-content--collapsed') ? false : true
         );
       }
 
