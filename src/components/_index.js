139837a5f36b4a69859f9db64e3ba6711b1aa2c5
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-09-08 21:13:25 +0200

    Added style to the input number buttons and add the js functionality

--- src/components/_index.js
+++ src/components/_index.js
@@ -1,5 +1,6 @@
 import './collapsible/collapsible.js'
 import './image_carousel/image_carousel.js'
+import './form/input/input.js'
 import './form/search/search.js'
 import './form/search_seasons/search_seasons.js'
 import './modal_box/modal_box.js'
---
+++ src/components/form/input/input.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const countInput = document.querySelector('.osg-input__input--number');
+
+  if (!countInput) {
+    return;
+  }
+
+  const btnUp = document.querySelector('.osg-input__counter-nav-button--up');
+  const btnDown = document.querySelector('.osg-input__counter-nav-button--down');
+  const countMin = countInput.min;
+  const countMax = countInput.max;
+
+  function increaseCount () {
+    let oldValue = parseFloat(countInput.value);
+    let newVal;
+
+    if (oldValue >= countMax) {
+      newVal = oldValue;
+    } else {
+      newVal = oldValue + 1;
+    }
+    countInput.value = newVal;
+  }
+
+  function decreaseCount () {
+    let oldValue = parseFloat(countInput.value);
+    let newVal;
+
+    if (oldValue <= countMin) {
+      newVal = oldValue;
+    } else {
+      newVal = oldValue - 1;
+    }
+    countInput.value = newVal;
+  }
+
+  btnUp.addEventListener('click', increaseCount, false);
+  btnDown.addEventListener('click', decreaseCount, false);
+});
