91e2392061e179263a83a25dc3e3c7629accdf8e
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-09-23 23:34:52 +0200

    Added keyboard navigation to search dropdown

--- src/components/form/search/search.vue
+++ src/components/form/search/search.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="osg-search osg-search--inline">
-    <div class="osg-search__form" aria-expanded="true" role="combobox" aria-haspopup="listbox" aria-owns="id-results">
-      <input class="osg-search__input" ref="input" :value="value" type="search" autocomplete="off" placeholder="Search" aria-label="Search" @keyup="$emit('change', $event.target.value)" v-on:keyup.enter="$emit('enter', $event.target.value)">
+    <div
+      class="osg-search__form"
+      role="combobox"
+      aria-expanded="true"
+      aria-haspopup="listbox"
+      aria-owns="id-results">
+      <input
+        v-on:keyup="$emit('change', $event.target.value)"
+        v-on:keyup.enter="$emit('enter', $event.target.value)"
+        v-on:keyup.down="setFocus($event)"
+        v-on:keyup.up="setFocus($event)"
+        :value="value"
+        class="osg-search__input"
+        type="search"
+        aria-label="Search"
+        ref="input"
+        autocomplete="off"
+        placeholder="Type a word">
     </div>
-    <ul v-if="items.length" class="osg-search__dropdown" role="listbox" id="id-results">
-      <li v-for="(item, index) of items" :key="index" role="option">
-        <a @click.prevent="itemClick(index)" class="osg-search__dropdown__item">{{ item }}</a>
+    <ul
+      v-if="items.length"
+      ref="list"
+      class="osg-search__dropdown"
+      id="id-results"
+      role="listbox">
+      <li
+        v-for="(item, index) of items" :key="index"
+        v-on:click.prevent="itemClick(index)"
+        v-on:keyup.enter.prevent="itemClick(index)"
+        v-on:keyup.down.prevent="setFocus($event)"
+        v-on:keyup.up.prevent="setFocus($event)"
+        :class="{ 'osg-search__dropdown__item--focus': index === focus }"
+        class="osg-search__dropdown__item"
+        role="option"
+        tabindex="0">
+        {{ item }}
       </li>
     </ul>
   </div>
@@ -30,12 +60,41 @@ export default {
       default: true
     }
   },
+  data() {
+    return {
+      focus: null
+    }
+  },
   methods: {
     itemClick(index) {
-      this.$emit('item', index)
+      this.$emit('item', index);
+      this.focus = null;
       if (this.inputFocusAfterItemClick) {
         this.$refs.input.focus()
       }
+    },
+    setFocus(event) {
+      const lastItem = this.items.length - 1;
+
+      switch (event.code) {
+        case 'ArrowUp':
+          if (this.focus === null || this.focus === 0) {
+            this.focus = lastItem;
+            this.$refs.list.lastChild.focus();
+          } else if (this.focus > 0) {
+            this.focus--;
+          }
+          break;
+        case 'ArrowDown':
+          if (this.focus === null || this.focus === lastItem) {
+            this.focus = 0;
+            this.$refs.list.firstChild.focus();
+          } else if (this.focus < lastItem) {
+            this.focus++;
+          }
+          break;
+      }
+      this.$refs.list.childNodes[this.focus].focus();
     }
   }
 }
