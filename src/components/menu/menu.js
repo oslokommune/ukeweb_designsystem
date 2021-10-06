4487aa2d4df02c1a07103d775f9bf216d1b50e15
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-10-06 23:01:59 +0200

    Added nondevmode menu code and expand collapse the menu

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -8,11 +8,18 @@ document.addEventListener("DOMContentLoaded", function () {
   hamburgerButtonTriggers.forEach((hamburgerButtonTrigger) => {
     hamburgerButtonTrigger.addEventListener("click", (event) => {
       let iconButton = event.target.querySelector(".osg-button-menu__icon");
+      let osgNavMenu = event.target.nextElementSibling;
 
       if (hamburgerButtonTrigger.getAttribute("aria-expanded") === "false") {
-        hamburgerButtonTrigger.setAttribute("aria-expanded", "true")
+        hamburgerButtonTrigger.setAttribute("aria-expanded", "true");
       } else {
         hamburgerButtonTrigger.setAttribute("aria-expanded", "false");
+      }
+
+      if ((osgNavMenu && osgNavMenu.style.display === "none") || osgNavMenu.style.display === "") {
+        osgNavMenu.style.display = "block";
+      } else {
+        osgNavMenu.style.display = "none";
       }
 
       iconButton.classList.toggle("osg-button-menu__icon--close");
