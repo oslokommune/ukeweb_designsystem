18f4899c19995b344fc89a335568084ec6d2445d
Lars Boldt <boldt.lars@gmail.com>
2021-10-08 14:52:00 +0200

    adds date component

--- src/components/form/date/date.js
+++ src/components/form/date/date.js
@@ -1,11 +1,39 @@
 import OsgDate from "./date.vue";
 
 document.addEventListener("DOMContentLoaded", function () {
-  let osgDatePlaceholder = document.getElementById("osg-date-vue");
-  if (osgDatePlaceholder) {
-    Vue.component("osg-date", OsgDate);
-    new Vue({
-      el: osgDatePlaceholder,
-    });
-  }
+  const osgDatePlaceholderElements = ["osg-date-vue", "osg-date-vue-dev"];
+  osgDatePlaceholderElements.forEach(function (elementName) {
+    let osgDatePlaceholder = document.getElementById(elementName);
+    if (osgDatePlaceholder) {
+      Vue.component("osg-date", OsgDate);
+      new Vue({
+        el: osgDatePlaceholder,
+
+        data: () => ({
+          date: null,
+          minDate: new Date("2020-10-01"),
+          maxDate: new Date("2022-10-15"),
+        }),
+
+        computed: {
+          displayDate() {
+            if (this.date) {
+              const day = this.date.getDate() > 9 ? this.date.getDate() : "0" + this.date.getDate();
+              const month = this.date.getMonth() + 1 > 9 ? this.date.getMonth() + 1 : "0" + (this.date.getMonth() + 1);
+              const year = this.date.getFullYear();
+
+              return `${day}.${month}.${year}`;
+            }
+            return null;
+          },
+        },
+
+        methods: {
+          setDate(date) {
+            this.date = date;
+          },
+        },
+      });
+    }
+  });
 });
--- src/components/form/date/date.vue
+++ src/components/form/date/date.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="osg-date">
     <label class="osg-date__label">
-      Pick a date
-      <input type="text" pattern="\d{2}-\d{2}-\d{4}" class="osg-date__input" :value="value" autocomplete="off" />
+      {{ label }}
+      <input type="text" class="osg-date__input" :value="displayDate" autocomplete="off" v-on:focus="toggleDatepicker(true)" />
     </label>
-    <nrk-core-datepicker class="osg-input-datepicker__datepicker" ref="datepicker" v-show="isDatepickerOpen" days="man,tir,ons,tor,fre,lør,søn">
-      <fieldset class="osg-input-datepicker__datepicker-nav">
-        <button class="osg-input-datepicker__datepicker-nav-button osg-input-datepicker__datepicker-nav-button--prev" :value="datepickerPaginateMonth(-1)" :disabled="datepickerPaginateMonthDisabled(-1)" aria-label="Forrige måned"></button>
-        <button class="osg-input-datepicker__datepicker-nav-button osg-input-datepicker__datepicker-nav-button--next" :value="datepickerPaginateMonth(+1)" :disabled="datepickerPaginateMonthDisabled(+1)" aria-label="Neste måned"></button>
+    <nrk-core-datepicker class="osg-date__datepicker" ref="datepicker" v-show="showDatepicker" :days="days" :months="months">
+      <fieldset class="osg-date__datepicker__nav">
+        <button class="osg-date__datepicker__button osg-date__datepicker__button--prev" :value="browseMonth(-1)" :disabled="browseMonthDisabled(-1)" :aria-label="btnPrevMonthLabel"></button>
+        <button class="osg-date__datepicker__button osg-date__datepicker__button--next" :value="browseMonth(+1)" :disabled="browseMonthDisabled(+1)" :aria-label="btnNextMonthLabel"></button>
       </fieldset>
-      <table class="osg-input-datepicker__datepicker-calendar"></table>
+      <table class="osg-date__datepicker__calendar"></table>
     </nrk-core-datepicker>
   </div>
 </template>
+
 <script>
 import CoreDatepicker from "@nrk/core-datepicker";
 window.customElements.define("nrk-core-datepicker", CoreDatepicker);
@@ -20,22 +21,101 @@ window.customElements.define("nrk-core-datepicker", CoreDatepicker);
 export default {
   name: "OsgDate",
   props: {
-    value: {
+    label: {
+      type: String,
+      default: "Pick a date",
+    },
+    date: {
+      type: Date,
+      default: null,
+    },
+    displayDate: {
       type: String,
       default: "",
+    },
+    min: {
+      type: Date,
+      default: () => {
+        return new Date("1900-01-01");
+      },
+    },
+    max: {
+      type: Date,
+      default: () => {
+        return new Date("2100-01-01");
+      },
+    },
+    days: {
+      type: String,
+      default: "man,tir,ons,tor,fre,lør,søn",
+    },
+    months: {
+      type: String,
+      default: "januar,februar,mars,april,mai,juni,juli,august,september,oktober,november,desember",
+    },
+    btnPrevMonthLabel: {
+      type: String,
+      default: "Forrige måned",
+    },
+    btnNextMonthLabel: {
+      type: String,
+      default: "Neste måned",
     },
   },
 
   data: () => ({
-    isDatepickerOpen: true,
+    datepicker: null,
+    showDatepicker: false,
+    browseDate: new Date(Date.now()),
   }),
 
-  mounted: function () {},
-  destroyed: function () {},
+  mounted() {
+    this.datepicker = this.$refs.datepicker;
+    this.datepicker.date = this.date ? this.date : new Date();
+    this.datepicker.disabled = (date) => {
+      return (this.max && date > this.max.setHours(23, 59, 59, 999)) || (this.min && date < this.min);
+    };
+
+    this.datepicker.addEventListener("datepicker.change", this.onDatepickerChange);
+    this.datepicker.addEventListener("datepicker.click.day", this.onDatepickerClickDay);
+    window.addEventListener("click", this.onDatepickerOutside);
+    window.addEventListener("keyup", this.onDatepickerOutside);
+  },
+
+  destroyed() {
+    this.datepicker.removeEventListener("datepicker.change", this.onDatepickerChange);
+    this.datepicker.removeEventListener("datepicker.click.day", this.onDatepickerClickDay);
+    window.removeEventListener("click", this.onDatepickerOutside);
+    window.removeEventListener("keyup", this.onDatepickerOutside);
+  },
 
   methods: {
-    datepickerPaginateMonth() {},
-    datepickerPaginateMonthDisabled() {},
+    browseMonth(direction) {
+      const next = new Date(this.browseDate).setMonth(this.browseDate.getMonth() + direction);
+      return Math.max(this.min || 0, Math.min(this.max.setHours(23, 59, 59, 999) || Infinity, next));
+    },
+    browseMonthDisabled(direction) {
+      if (this.datepicker) {
+        let next = new Date(this.browseDate).setMonth(this.browseDate.getMonth() + direction);
+        next = new Date(next).setDate(1);
+        return (direction > 0 && next > this.max) || next < this.min;
+      }
+    },
+    toggleDatepicker(bool) {
+      this.showDatepicker = bool;
+    },
+    onDatepickerOutside(event) {
+      if (!this.$el.contains(event.target)) {
+        this.toggleDatepicker(false);
+      }
+    },
+    onDatepickerClickDay() {
+      this.$emit("set", this.datepicker.date);
+      this.toggleDatepicker(false);
+    },
+    onDatepickerChange(event) {
+      this.browseDate = event.detail;
+    },
   },
 };
 </script>
