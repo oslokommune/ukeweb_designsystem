dc83f53bbccae394f38bc07be979ffef2fdc67db
Cristina Menduinya <cristina.menduina@uke.oslo.kommune.no>
2021-11-18 12:09:56 +0100

    Update functionality of vue component in search seasons

--- src/components/form/search_seasons/search_seasons.init.js
+++ src/components/form/search_seasons/search_seasons.init.js
@@ -1,5 +1,6 @@
-import { OsgSearchSeasons } from "./search_seasons.js";
+import { OsgSearchSeasonsJs, OsgSearchSeasonsInit } from "./search_seasons.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  OsgSearchSeasons();
+  OsgSearchSeasonsJs();
+  OsgSearchSeasonsInit();
 });
--- src/components/form/search_seasons/search_seasons.js
+++ src/components/form/search_seasons/search_seasons.js
@@ -1,4 +1,7 @@
-export function OsgSearchSeasons() {
+import Vue from "vue";
+import OsgSearchSeasons from "./search_seasons.vue";
+
+export function OsgSearchSeasonsJs() {
   let searchSeasonsDropdownTriggers = document.querySelectorAll(".components-form-search-seasons-dropdown");
   if (searchSeasonsDropdownTriggers) {
     searchSeasonsDropdownTriggers.forEach((trigger) => {
@@ -17,3 +20,78 @@ export function OsgSearchSeasons() {
     });
   }
 }
+
+export const OsgSearchSeasonsInit = () => {
+  const container = document.getElementById("osg-search-seasons");
+  if (container) {
+    new Vue({
+      el: container,
+      components: {
+        OsgSearchSeasons
+      },
+      mounted() {
+        this.event("input-change", this.value);
+      },
+      data: () => ({
+        value: "h",
+        items: [],
+        season: {
+          title: "What are you looking for today?",
+          endpoint: "http://oslo.kommune.local/sokeresultater/",
+          input: {
+            id: "search-seasons-1",
+            name: "osg-search-seasons-vue",
+            placeholder: "Type a word"
+          },
+          ariaLabel: "Search",
+          heroFigure: {
+            defaultUrl: "https://ukeweb-public.s3.eu-central-1.amazonaws.com/images/800x800/1.jpg",
+            alt: "A lighthouse in the sea"
+          },
+          theme: "green-dark",
+          hiddenFields: [
+            {
+              name: "name-1",
+              value: "value-1"
+            }
+          ],
+          imageShape: "circle",
+          shapePosition: "right"
+        }
+      }),
+      methods: {
+        event(event, payload) {
+          if (event === "input-change") {
+            if (payload[0] && payload[0].toLowerCase() === "h") {
+              this.items = [
+                {
+                  text: "Hi",
+                },
+                {
+                  text: "Hello",
+                },
+                {
+                  text: "Halo",
+                },
+                {
+                  text: "Hei",
+                  subtext: "Greetings",
+                }
+              ];
+            } else {
+              this.items = [];
+            }
+            this.value = payload;
+          } else if (event === "item-select") {
+            this.value = this.items[payload].text;
+            this.items = [];
+          } else if (event === "itemlist-blur") {
+            this.items = [];
+          } else if (event === "submit") {
+            console.log(payload);
+          }
+        },
+      },
+    });
+  }
+};
