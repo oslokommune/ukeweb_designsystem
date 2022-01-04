3f77f3362c8dde17035d22ef2a19d18a8ccb0bc0
jeanettelandoyaune <86772169+jeanettelandoyaune@users.noreply.github.com>
2022-01-04 15:49:03 +0100

    added counter to textarea

--- src/components/_index.js
+++ src/components/_index.js
@@ -9,3 +9,4 @@ import "./map/map.js";
 import "./menu/menu.init.js";
 import "./pagination/pagination.init.js";
 import "./slide_panel/slide_panel.init.js";
+import "./form/textarea/textarea.init.js";
---
+++ src/components/form/textarea/textarea.init.js
@@ -0,0 +1,5 @@
+import { OsgTextArea } from "./textarea.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  OsgTextArea();
+});
