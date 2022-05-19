b56c5f1fc49fd25c0b0bfaac317ff8e62c55bf6d
Knut Erik Berg-Hansen <kbergha@gmail.com>
2022-05-19 15:15:24 +0200

    Removes last displayed popup when data is cleared.

--- src/components/map/map.vue
+++ src/components/map/map.vue
@@ -61,6 +61,7 @@ export default {
   data: () => ({
     layerIds: [],
     dataSourceIds: [],
+    lastDisplayedPopup: null,
     mapObject: null,
     mapReady: false,
     showPopups: true,
@@ -209,15 +210,19 @@ export default {
     clearMapAndData() {
       var _this = this; // Scope this, bobby!
 
-      this.layerIds.forEach((element) => {
-        if (_this.mapObject.getLayer(element)) {
-          _this.mapObject.removeLayer(element);
+      if (this.lastDisplayedPopup !== null) {
+        this.lastDisplayedPopup.remove();
+      }
+
+      this.layerIds.forEach((layerId) => {
+        if (_this.mapObject.getLayer(layerId)) {
+          _this.mapObject.removeLayer(layerId);
         }
       });
 
-      this.dataSourceIds.forEach((element) => {
-        if (_this.mapObject.getSource(element)) {
-          _this.mapObject.removeSource(element);
+      this.dataSourceIds.forEach((dataSourceId) => {
+        if (_this.mapObject.getSource(dataSourceId)) {
+          _this.mapObject.removeSource(dataSourceId);
         }
       });
 
@@ -598,7 +603,8 @@ export default {
         let html = _this.$_getPopupHtml(event.features[0]);
 
         if (typeof html === "string") {
-          new maplibregl.Popup({ className: "osg-map__popup" }).setLngLat(event.lngLat).setHTML(html).addTo(_this.mapObject);
+          const popup = new maplibregl.Popup({ className: "osg-map__popup" }).setLngLat(event.lngLat).setHTML(html).addTo(_this.mapObject);
+          this.lastDisplayedPopup = popup;
         }
       });
 
