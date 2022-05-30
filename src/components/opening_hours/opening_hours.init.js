83bc999d5c25e5adb97b91c03b2484e71662f7df
Lars Boldt <boldt.lars@gmail.com>
2022-05-30 15:39:10 +0200

    removes opening hours as a styleguide component

--- src/components/opening_hours/opening_hours.init.js
+++
@@ -1,9 +0,0 @@
-import { OsgOpeningHours } from "./opening_hours";
-
-document.addEventListener("opening-hours__trigger", function (payload) {
-  console.log(payload.detail);
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  OsgOpeningHours();
-});
