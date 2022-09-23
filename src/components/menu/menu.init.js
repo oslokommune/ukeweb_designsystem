e7ffe576cd29403abacee8f63caba64a20c48067
Lars Boldt <boldt.lars@gmail.com>
2022-09-23 13:53:14 +0200

    add check for element

--- src/components/menu/menu.init.js
+++ src/components/menu/menu.init.js
@@ -4,20 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
   OsgMenu.init();
 
   const menu = document.getElementById("osg-menu-1");
-  menu.addEventListener("OsgMenuOpen", (e) => {
-    console.log("Menu opened", e.detail);
-  });
-  menu.addEventListener("OsgMenuClose", (e) => {
-    console.log("Menu closed", e.detail);
-  });
+  if (menu) {
+    menu.addEventListener("OsgMenuOpen", (e) => {
+      console.log("Menu opened", e.detail);
+    });
+    menu.addEventListener("OsgMenuClose", (e) => {
+      console.log("Menu closed", e.detail);
+    });
+  }
 
   const menu2 = document.getElementById("osg-menu-2");
-  const btnOpen = document.getElementById("osg-menu-2-open");
-  const btnClose = document.getElementById("osg-menu-2-close");
-  btnOpen.addEventListener("click", (e) => {
-    OsgMenu.open(menu2);
-  });
-  btnClose.addEventListener("click", (e) => {
-    OsgMenu.close(menu2);
-  });
+  if (menu2) {
+    const btnOpen = document.getElementById("osg-menu-2-open");
+    const btnClose = document.getElementById("osg-menu-2-close");
+    btnOpen.addEventListener("click", (e) => {
+      OsgMenu.open(menu2);
+    });
+    btnClose.addEventListener("click", (e) => {
+      OsgMenu.close(menu2);
+    });
+  }
 });
