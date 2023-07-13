b74722a6c41778822d254029990b6c26507a77a9
jeanettelandoyaune <86772169+jeanettelandoyaune@users.noreply.github.com>
2023-07-13 14:20:56 +0200

    WIP and fix to fieldset

--- src/components/form/checkbox/checkbox.init.js
+++ src/components/form/checkbox/checkbox.init.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import OsgCheckbox from "./checkbox.vue";
+import { OsgCheckbox1 } from "./checkbox.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+  OsgCheckbox1();
   if (document.getElementById("osg-checkbox-vue-1")) {
     Vue.component("osg-checkbox", OsgCheckbox);
     new Vue({ el: "#osg-checkbox-vue-1" });
