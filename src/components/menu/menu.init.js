2185e7926544db4733ea8e86f32101091272b342
jonaslindevall <jonas.lindevall@uke.oslo.kommune.no>
2023-08-02 11:44:00 +0200

    stylelint no-descending fixes and console.logs

--- src/components/menu/menu.init.js
+++ src/components/menu/menu.init.js
@@ -3,16 +3,6 @@ import OsgMenu from './menu';
 document.addEventListener('DOMContentLoaded', () => {
   OsgMenu.init();
 
-  const menu = document.getElementById('osg-menu-1');
-  if (menu) {
-    menu.addEventListener('OsgMenuOpen', (e) => {
-      console.log('Menu opened', e.detail); // console.log in production = bad
-    });
-    menu.addEventListener('OsgMenuClose', (e) => {
-      console.log('Menu closed', e.detail); // console.log in production = bad
-    });
-  }
-
   const menu2 = document.getElementById('osg-menu-2');
   if (menu2) {
     const btnOpen = document.getElementById('osg-menu-2-open');
