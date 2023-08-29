a3bb5b97b2a555ef900403708b6a192dd0f7e780
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2023-08-29 08:54:06 +0200

    renames 'element' for clarity and adds error handling

--- src/utils/js/events/dispatchCustomEvent.js
+++ src/utils/js/events/dispatchCustomEvent.js
@@ -1,12 +1,16 @@
 /**
  * Custom dispatch event function. Call this instead of new CustomEvent.
  */
-function dispatchCustomEvent(eventName, eventDetail, element) {
+function dispatchCustomEvent(eventName, eventDetail, eventObject) {
+  if (typeof eventObject.dispatchEvent !== 'function') {
+    throw new Error('invalid event target');
+  }
+
   const event = new CustomEvent(eventName, {
     detail: eventDetail,
   });
 
-  element.dispatchEvent(event);
+  eventObject.dispatchEvent(event);
 }
 
 export default dispatchCustomEvent;
