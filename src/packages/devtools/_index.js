8632653174b626cbe897e3ea010dcc4abd0c2e19
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2023-09-15 08:48:28 +0200

    remove unused import after deleting oslo
    Signed-off-by: Jonas Lindevall <jonas.h.lindevall@gmail.com>

--- src/packages/devtools/_index.js
+++ src/packages/devtools/_index.js
@@ -3,7 +3,6 @@ import Vue from 'vue';
 import '../../general/_index';
 import '../../getting_started/_index';
 import '../../components/_index';
-import '../../pages/_index';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
