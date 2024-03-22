93abd28712ece954b0cb3945a189345130e3141f
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2024-03-22 11:07:42 +0100

    change structure

--- src/components/pagination/pagination.init.js
+++ src/components/pagination/pagination.init.js
@@ -5,7 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
   if (document.getElementById('vue-pagination')) {
     const app = createApp({
       name: 'OdsPaginationApp',
-      components: { OdsPagination },
+      components: {
+        OdsPagination,
+      },
       data: () => ({
         currentIndex: 99,
         limit: 2,
