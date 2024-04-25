20f3f0d4bbf330be90bb897c13b275f36045c987
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2024-04-25 14:12:27 +0200

    inital svgtofont attempt

---
+++ .svgtofontrc.js
@@ -0,0 +1,14 @@
+export default {
+  fontName: 'OsloIcons2',
+  css: {
+    output: 'src/system/',
+    fileName: '_icon-map2.scss',
+  },
+  template: 'src/utils/icon-map.scss.njk',
+  startUnicode: 0xea01,
+  svgicons2svgfont: {
+    fontHeight: 512,
+    normalize: true,
+  },
+  website: false,
+};
