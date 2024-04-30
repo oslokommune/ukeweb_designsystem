5f6954b5fc74088b38b51257f2ab82856336720a
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2024-04-30 09:20:32 +0200

    remove test instance of checkbox component

--- src/components/form/checkbox/checkbox.init.js
+++ src/components/form/checkbox/checkbox.init.js
@@ -3,7 +3,6 @@ import OdsCheckbox from './checkbox.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   const checkboxElement1 = document.getElementById('ods-checkbox-vue-1');
-  const checkboxElement2 = document.getElementById('ods-checkbox-vue-2');
 
   if (checkboxElement1) {
     const app1 = createApp({
@@ -13,15 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
       },
     });
     app1.mount(checkboxElement1);
-  }
-
-  if (checkboxElement2) {
-    const app2 = createApp({
-      name: 'OdsCheckboxApp',
-      components: {
-        OdsCheckbox,
-      },
-    });
-    app2.mount(checkboxElement2);
   }
 });
