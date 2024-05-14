0b65fc6b199bb23a6b66fac6d36e7d1cc4d8257b
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2024-05-14 09:24:39 +0200

    fix focus outline on checkbox component

--- src/components/form/checkbox/checkbox.init.js
+++ src/components/form/checkbox/checkbox.init.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue';
 import OdsCheckbox from './checkbox.vue';
-import OdsCheckboxEvents from './checkboxEvents';
 
 document.addEventListener('DOMContentLoaded', () => {
-  OdsCheckboxEvents.init();
   const checkboxElement = document.getElementById('ods-checkbox-vue-1');
 
   if (checkboxElement) {
