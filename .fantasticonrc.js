caa7416f07175d2163bda9ff58b1216a66cbc8d3
Jonas Lindevall <49975790+jonaashl@users.noreply.github.com>
2025-09-11 23:18:18 +0200

    ODS-75: Remove webfont, add fantasticon (#547)
    * add fantasticon and remove webfont to resolve security vulns
    
    * remove comments

---
+++ .fantasticonrc.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const root = __dirname;
+
+module.exports = {
+  inputDir: path.join(root, 'src/assets/icons'),
+  outputDir: path.join(root, 'src/assets/fonts'),
+
+  name: 'OsloIcons',
+  fontHeight: 512,
+  normalize: true,
+
+  fontTypes: ['eot', 'ttf', 'woff', 'woff2'],
+  assetTypes: ['scss'],
+
+  pathOptions: {
+    scss: path.join(root, 'src/system/_icon-map.scss')
+  },
+
+  templates: {
+    scss: path.join(root, 'src/utils/_icon-map.scss.hbs')
+  }
+};
